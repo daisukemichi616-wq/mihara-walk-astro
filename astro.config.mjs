053c7372d3d1139cfafc77b58f6b266294e9ad75
michim <dk_mk123@yahoo.co.jp>
2026-03-27 22:36:38 +0900

    b

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ▼ これが絶対に必要です ▼
+  site: 'https://photomap-mihara-walk.pages.dev',
+  
+  integrations: [tailwind(), sitemap()],
+});
